0c719b5c7936f57e15888e1ef15492c5b2daa1d8
rajeev-k47 <rajeevkumawat2007@outlook.com>
2026-06-21 00:02:38 +0530

    <Init: Conf update>?

---
+++ fish/config.fish
@@ -0,0 +1,44 @@
+if status is-interactive
+    # Starship custom prompt
+    starship init fish | source
+
+    # Direnv + Zoxide
+    command -v direnv &>/dev/null && direnv hook fish | source
+    command -v zoxide &>/dev/null && zoxide init fish --cmd cd | source
+
+    # Better ls
+    alias ls='eza --icons --group-directories-first -1'
+    alias bt='btop -c ~/.local/share/caelestia/btop/btop.conf --force-utf'
+    # Abbrs
+    abbr gd 'git diff'
+    abbr ga 'git add .'
+    abbr gc 'git commit -am'
+    abbr gl 'git log'
+    abbr gs 'git status'
+    abbr gst 'git stash'
+    abbr gsp 'git stash pop'
+    abbr gp 'git push'
+    abbr gpl 'git pull'
+    abbr gsw 'git switch'
+    abbr gsm 'git switch main'
+    abbr gb 'git branch'
+    abbr gbd 'git branch -d'
+    abbr gco 'git checkout'
+    abbr gsh 'git show'
+
+    abbr l ls
+    abbr ll 'ls -l'
+    abbr la 'ls -a'
+    abbr lla 'ls -la'
+
+    set -x SHELL /bin/bash
+    # Custom colours
+    cat ~/.local/state/caelestia/sequences.txt 2>/dev/null
+
+    # For jumping between prompts in foot terminal
+    function mark_prompt_start --on-event fish_prompt
+        echo -en "\e]133;A\e\\"
+    end
+end
+fish_add_path "$HOME/.ktorite"
+fish_add_path /home/rajeev/flutter/bin
